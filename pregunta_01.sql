f89fa2004a6401d7fc1f41268fad3f4b317f42d0
scastanedab <101910033+scastanedab@users.noreply.github.com>
2022-04-11 20:14:15 -0500

    Update pregunta_01.sql
    Pregunta 1

--- pregunta_01.sql
+++ pregunta_01.sql
@@ -17,4 +17,33 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 
-  SELECT sum(c12) FROM tbl1
+import pandas as pd
+
+import sqlite3
+
+conn = sqlite3.connect(":memory:")  ## aca se indica el nombre de la db.
+cur = conn.cursor()
+
+with open("tbl1.csv", "rt") as f:
+    datos = f.readlines()
+
+datos = [line.replace('\n', '') for line in datos]
+datos = [line.split(",") for line in datos]
+datos = [tuple(line) for line in datos]
+
+conn.executescript(
+    """
+DROP TABLE IF EXISTS tbl1;
+
+CREATE TABLE tbl1 (K0  CHAR(1), K1  INT, c12 FLOAT, c13 INT, c14 DATE, c15 FLOAT, c16 CHAR(4));
+"""
+)
+conn.commit()
+
+#cur.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()
+
+cur.executemany("INSERT INTO tbl1 VALUES (?,?,?,?,?,?,?)", datos)
+
+#cur.execute("SELECT * FROM tbl1 LIMIT 1;").fetchall()
+
+pd.DataFrame({'SUM(c12)':cur.execute("SELECT sum(c12) FROM tbl1").fetchall()})
