1cdcab7ad926f9361be13a6176fdf3a87720d6c0
scastanedab <101910033+scastanedab@users.noreply.github.com>
2022-04-11 19:59:10 -0500

    Update pregunta_01.sql
    Pregunta 1

--- pregunta_01.sql
+++ pregunta_01.sql
@@ -17,4 +17,36 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 
--- pd.DataFrame({'SUM(c12)':cur.execute("SELECT sum(c12) FROM tbl1").fetchall()})
+--
+with open("tbl1.csv", "rt") as f:
+  data = f.readlines()
+
+
+conn.executescript(
+    """
+DROP TABLE IF EXISTS tbl1;
+
+CREATE TABLE tbl1(K0       CHAR(1),
+                  K1       INT,
+                  c12      FLOAT,
+                  c13      INT,
+                  c14      DATE,
+                  c15      FLOAT,
+                  c16      CHAR(4));
+"""
+)
+conn.commit()
+
+cur.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall() #para ver las tablas creadas
+
+data = [line.replace('\n', '') for line in data]
+data = [line.split(",") for line in data]
+data = [tuple(line) for line in data]
+
+cur.executemany("INSERT INTO tbl1 VALUES (?,?,?,?,?,?,?)", data)
+
+cur.execute("SELECT * FROM tbl1 LIMIT 1;").fetchall() #para verificar el primer registro 
+
+#cur.execute("SELECT sum(c12) FROM tbl1").fetchall()
+
+pd.DataFrame({'SUM(c12)':cur.execute("SELECT sum(c12) FROM tbl1").fetchall()})
