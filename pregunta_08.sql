074bc7c140d2f3dbd441bbed15d8222eb107df77
scastanedab <101910033+scastanedab@users.noreply.github.com>
2022-04-21 23:27:56 -0500

    Update pregunta_08.sql
    08

--- pregunta_08.sql
+++ pregunta_08.sql
@@ -41,6 +41,4 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --
-SELECT  YEAR(c23), avg(c21) 
-FROM tbl2 
-GROUP BY YEAR(c23);
+SELECT strftime('%Y',c23), avg(c21) FROM tbl2 GROUP BY strftime('%Y',c23);
